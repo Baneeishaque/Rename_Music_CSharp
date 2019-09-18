4b8854dfa88e3ca87096a4bffaf53da804e476c4
Banee Ishaque K <baneeishaque@gmail.com>
2019-09-18 19:40:14 +0530

    In

---
+++ Rename_Music/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Common.Functions.ListFilesUnderDirectory;
+
+namespace Rename_Music
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var item in DirectoryFileLister.ListFiles(
+                @"G:\DK-HP-PA-2000AR\Songs\Records\Pitbull\Global Warming"))
+                Console.WriteLine(item);
+        }
+    }
+}
